18e098dbba8822635d84a1123debdacce4660602
lucile bessac <lucilebessac@free.fr>
2024-04-21 12:41:16 +0200

    Ajout script et images des 100 premiers posts pour exemple

--- Scripts/img_scrapper.py
+++ Scripts/img_scrapper.py
@@ -11,7 +11,7 @@ reddit = praw.Reddit(client_id="14JBbvpP01tPby97id6m4w",
 subreddit_name = "tumblr"
 data_path = "../Data/Raw/Images"
 # Dossier de stockage des images
-if not os.path.exists(subreddit_name):
+if not os.path.exists(data_path):
     os.makedirs(data_path)
 
 # Objet subreddit
@@ -20,14 +20,14 @@ subreddit = reddit.subreddit(subreddit_name)
 print("En train de scrapper le subreddit :", subreddit_name)
 
 # Pour chacun des 10 dernier posts
-for post in subreddit.new(limit=10):
+for post in subreddit.new(limit=100):
     # Si le post est une image
     if post.url.endswith(('.jpg', '.jpeg', '.png')):
         image_url = post.url
         # Récupère l'image
         image = requests.get(image_url).content
         # Sauvegarde l'image
-        with open(os.path.join(subreddit_name, f"{post.id}.jpg"), "wb") as f:
+        with open(os.path.join(data_path, f"{post.id}.jpg"), "wb") as f:
             f.write(image)
         print("Image du post", post.id, "enregistrée dans le fichier",post.id,".jpg")
 print("Images enregistrées dans le dossier :", data_path)
